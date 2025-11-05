0adfd850d5b57ba0a45695a9f9f7ad5d885edeb3
Evgeniy Dmitrienko <dmitrienko.evgeniy@gmail.com>
2025-11-05 13:08:58 +0700

    fix: change js for menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,18 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
 
-const closeMenu = () => {
-    nav.classList.toggle('open');
-    burger.classList.toggle('open');
-    document.body.classList.toggle('no-scroll');
-};
-
-if (burger && nav) {
+if (burger) {
   burger.addEventListener('click', () => {
-      closeMenu();
+      nav.classList.toggle('open');
+      burger.classList.toggle('open');
+      document.body.classList.toggle('no-scroll');
   });
 
   nav.addEventListener('click', e => {
     if (e.target.matches('a')) {
-      closeMenu();
+      document.body.classList.remove('no-scroll');
+      nav.classList.remove('open');
+      burger.classList.remove('open');
     }
   });
 }
